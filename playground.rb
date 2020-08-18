6b95b41ea7eb8d3759b2bd45f55b523b66a2c884
Albert <albertwu0001@gmail.com>
2020-08-18 16:28:32 -0700

    define two functions of teaser and guess for words

--- playground.rb
+++ playground.rb
@@ -6,21 +6,41 @@ class Hangman
 
   def words
     [
-      ["cricket", "A game played by gentlemen"].
-      ["jogging", "We are not walking..."].
-      ["celebrate", "Remembering special moments"].
-      ["continent", "There are 7 of these"].
+      ["cricket", "A game played by gentlemen"],
+      ["jogging", "We are not walking..."],
+      ["celebrate", "Remembering special moments"],
+      ["continent", "There are 7 of these"],
       ["exotic", "Not from around here..."],
     ]
   end
 
-  def begin
-    puts "New game started... your clue is #{ @word.first}"
+  def print_teaser
+    word_teaser = ""
+
+    @word.first.size.times do
+      word_teaser += "_ "
+    end
+
+    puts word_teaser
+  end
+
+  def make_guess
+    puts "Enter a letter"
+
     guess = gets.chomp
-    puts "You guess #{guess}"
+  end
+
+  def begin
+    puts "New game started... your word is #{ @word.first.size} characters long"
+
+    print_teaser
+
+    puts "Clue: is #{ @word.last}"
+
+    make_guess
   end
 
 end
 
-game = Hangman.New
+game = Hangman.new
 game.begin
