606cc5dd034a2c758c5ab59f4cd973d68e95e93b
Albert <albertwu0001@gmail.com>
2020-08-18 16:19:44 -0700

    setup begin status and begin game play

--- playground.rb
+++ playground.rb
@@ -1,16 +1,26 @@
 class Hangman
   def initialize
-    letters = ('a'..'z').to_a
-    word = words.sample
+    @letters = ('a'..'z').to_a
+    @word = words.sample
   end
 
   def words
     [
-      "cricket", "A game played by gentlemen"
+      ["cricket", "A game played by gentlemen"].
+      ["jogging", "We are not walking..."].
+      ["celebrate", "Remembering special moments"].
+      ["continent", "There are 7 of these"].
+      ["exotic", "Not from around here..."],
     ]
   end
 
   def begin
+    puts "New game started... your clue is #{ @word.first}"
+    guess = gets.chomp
+    puts "You guess #{guess}"
   end
 
 end
+
+game = Hangman.New
+game.begin
