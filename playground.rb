9e66c08d0b6a655409a36be5cd9fd3bf4d188e23
Albert <albertwu0001@gmail.com>
2020-08-18 16:45:12 -0700

    add undate function to replace blank values with letter if mathes letter in word

--- playground.rb
+++ playground.rb
@@ -3,6 +3,11 @@ class Hangman
     @letters = ('a'..'z').to_a
     @word = words.sample
     @lives = 7
+    @correct_guesses = []
+    @word_teaser = ""
+    @word.first.size.times do
+      @word_teaser += "_ "
+    end
   end
 
   def words
@@ -15,14 +20,16 @@ class Hangman
     ]
   end
 
-  def print_teaser
-    word_teaser = ""
+  def print_teaser last_guess
+    update_teaser unless last_guess.nil?
+    puts @word_teaser
+  end
 
-    @word.first.size.times do
-      word_teaser += "_ "
+  def update_teaser
+    new_teaser = @word_teaser.split
+    new_teaser.each_with_index do |letter, index|
+      if letter == '_'
     end
-
-    puts word_teaser
   end
 
   def make_guess
@@ -32,14 +39,19 @@ class Hangman
 
       good_guess = @word.first.include? guess
       if good_guess
-        puts "Good guess!"
+        puts "Your are correct!"
+        @correct_guesses << guess
+        @letters.delete guess
+
+        print_teaser guess
+        make_guess
       else
         @lives -= 1
         puts "Sorry... you have #{ @lives } lives left. Try again"
         make_guess
       end
     else
-      puts "Game over!"
+      puts "Game over... better luck next time"
     end
 
   end
